26fb26f41a48d3dffe4efd37f9193ba3ec655d35
juhomakkonen <juho.makkonen@turku.fi>
2023-01-17 15:42:36 +0200

    Add own component for counter markers

--- src/components/EcoCounter/EcoCounterMarkers/EcoCounterMarkers.js
+++ src/components/EcoCounter/EcoCounterMarkers/EcoCounterMarkers.js
@@ -1,26 +1,15 @@
-import { PropTypes } from 'prop-types';
 import React, { useContext, useEffect, useState } from 'react';
 import { useMap } from 'react-leaflet';
-import { useSelector } from 'react-redux';
-import ecoCounterIcon from 'servicemap-ui-turku/assets/icons/icons-icon_ecocounter.svg';
-import ecoCounterIconBw from 'servicemap-ui-turku/assets/icons/contrast/icons-icon_ecocounter-bw.svg';
 import MobilityPlatformContext from '../../../context/MobilityPlatformContext';
-import { useAccessibleMap } from '../../../redux/selectors/settings';
-import { createIcon, isDataValid } from '../../MobilityPlatform/utils/utils';
+import { isDataValid } from '../../MobilityPlatform/utils/utils';
 import { fetchTrafficCounterStations } from '../EcoCounterRequests/ecoCounterRequests';
+import CounterMarkers from '../CounterMarkers';
 import EcoCounterContent from '../EcoCounterContent';
 
-const EcoCounterMarkers = ({ classes }) => {
+const EcoCounterMarkers = () => {
   const [ecoCounterStations, setEcoCounterStations] = useState([]);
 
   const { openMobilityPlatform, showEcoCounter } = useContext(MobilityPlatformContext);
-
-  const useContrast = useSelector(useAccessibleMap);
-
-  const { Marker, Popup } = global.rL;
-  const { icon } = global.L;
-
-  const customIcon = icon(createIcon(useContrast ? ecoCounterIconBw : ecoCounterIcon));
 
   useEffect(() => {
     if (openMobilityPlatform) {
@@ -46,26 +35,16 @@ const EcoCounterMarkers = ({ classes }) => {
     <>
       {renderData ? (
         ecoCounterStations.map(item => (
-          <Marker key={item.id} icon={customIcon} position={[item.lat, item.lon]}>
-            <div className={classes.popupWrapper}>
-              <Popup className="ecocounter-popup">
-                <div className={classes.popupInner}>
-                  <EcoCounterContent
-                    stationId={item.id}
-                    stationName={item.name}
-                  />
-                </div>
-              </Popup>
-            </div>
-          </Marker>
+          <CounterMarkers key={item.id} counterStation={item}>
+            <EcoCounterContent
+              stationId={item.id}
+              stationName={item.name}
+            />
+          </CounterMarkers>
         ))
       ) : null}
     </>
   );
-};
-
-EcoCounterMarkers.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
 export default EcoCounterMarkers;
--- src/components/EcoCounter/LamCounters/LamCounters.js
+++ src/components/EcoCounter/LamCounters/LamCounters.js
@@ -1,26 +1,15 @@
-import { PropTypes } from 'prop-types';
 import React, { useContext, useEffect, useState } from 'react';
 import { useMap } from 'react-leaflet';
-import { useSelector } from 'react-redux';
-import ecoCounterIcon from 'servicemap-ui-turku/assets/icons/icons-icon_ecocounter.svg';
-import ecoCounterIconBw from 'servicemap-ui-turku/assets/icons/contrast/icons-icon_ecocounter-bw.svg';
 import MobilityPlatformContext from '../../../context/MobilityPlatformContext';
-import { useAccessibleMap } from '../../../redux/selectors/settings';
-import { createIcon, isDataValid } from '../../MobilityPlatform/utils/utils';
+import { isDataValid } from '../../MobilityPlatform/utils/utils';
 import { fetchTrafficCounterStations } from '../EcoCounterRequests/ecoCounterRequests';
+import CounterMarkers from '../CounterMarkers';
 import LamCountersContent from './components/LamCountersContent';
 
-const LamCounters = ({ classes }) => {
+const LamCounters = () => {
   const [lamCounterStations, setLamCounterStations] = useState([]);
 
   const { openMobilityPlatform, showLamCounter } = useContext(MobilityPlatformContext);
-
-  const useContrast = useSelector(useAccessibleMap);
-
-  const { Marker, Popup } = global.rL;
-  const { icon } = global.L;
-
-  const customIcon = icon(createIcon(useContrast ? ecoCounterIconBw : ecoCounterIcon));
 
   useEffect(() => {
     if (openMobilityPlatform) {
@@ -46,26 +35,16 @@ const LamCounters = ({ classes }) => {
     <>
       {renderData ? (
         lamCounterStations.map(item => (
-          <Marker key={item.id} icon={customIcon} position={[item.lat, item.lon]}>
-            <div className={classes.popupWrapper}>
-              <Popup className="ecocounter-popup">
-                <div className={classes.popupInner}>
-                  <LamCountersContent
-                    stationId={item.id}
-                    stationName={item.name}
-                  />
-                </div>
-              </Popup>
-            </div>
-          </Marker>
+          <CounterMarkers key={item.id} counterStation={item}>
+            <LamCountersContent
+              stationId={item.id}
+              stationName={item.name}
+            />
+          </CounterMarkers>
         ))
       ) : null}
     </>
   );
-};
-
-LamCounters.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
 export default LamCounters;
