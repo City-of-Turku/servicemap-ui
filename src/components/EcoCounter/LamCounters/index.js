715dc8573e013da40ac2ed0d5ee38ae15e2403ce
juhomakkonen <juho.makkonen@turku.fi>
2023-01-30 14:57:34 +0200

    Clean up styles and index files

--- src/components/EcoCounter/LamCounters/index.js
+++ src/components/EcoCounter/LamCounters/index.js
@@ -1,5 +1,3 @@
-import { withStyles } from '@material-ui/core';
 import LamCounters from './LamCounters';
-import styles from './styles';
 
-export default withStyles(styles)(LamCounters);
+export default LamCounters;
