9d901e7a6553fabaeb26276fa44147b007bc2cde
juhomakkonen <juho.makkonen@turku.fi>
2023-02-21 12:46:02 +0200

    Ignore useEffect depedency warning

--- src/components/MobilityPlatform/PublicToilets/PublicToilets.js
+++ src/components/MobilityPlatform/PublicToilets/PublicToilets.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useContext } from 'react';
 import { useMap } from 'react-leaflet';
 import { useSelector } from 'react-redux';
