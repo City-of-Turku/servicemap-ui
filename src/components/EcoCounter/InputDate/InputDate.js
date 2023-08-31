593c1b81b66d4e85b5e6cfa6b9380a8db2591593
juhomakkonen <juho.makkonen@turku.fi>
2023-08-31 11:31:02 +0300

    Update input element to use MUI

--- src/components/EcoCounter/InputDate/InputDate.js
+++ src/components/EcoCounter/InputDate/InputDate.js
@@ -1,23 +1,33 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
+import { InputBase, InputAdornment } from '@mui/material';
+import styled from '@emotion/styled';
+import { CalendarMonth } from '@mui/icons-material';
 
-const InputDate = ({
-  classes, value, onClick, onChange,
-}, ref) => (
-  <input
-    className={classes.input}
+const InputDate = ({ value, onClick, onChange }, ref) => (
+  <StyledInputBase
     type="text"
     value={value}
     ref={ref}
-    onChange={(e) => onChange(e.target.value)}
+    onChange={e => onChange(e.target.value)}
     onClick={onClick}
+    startAdornment={(
+      <InputAdornment position="start">
+        <CalendarMonth />
+      </InputAdornment>
+    )}
   />
 );
 
+const StyledInputBase = styled(InputBase)(() => ({
+  fontSize: '0.913rem',
+  fontWeight: 'bold',
+  boxSizing: 'border-box',
+  backgroundColor: '#fff',
+  cursor: 'pointer',
+}));
+
 InputDate.propTypes = {
-  classes: PropTypes.shape({
-    input: PropTypes.string,
-  }).isRequired,
   value: PropTypes.string,
   onClick: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
