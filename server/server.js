08ce869f7a2e7bce96e1d24f79f2f4d3ce746f0d
juhomakkonen <juho.makkonen@turku.fi>
2024-02-21 12:34:36 +0200

    Small update to server file

--- server/server.js
+++ server/server.js
@@ -272,8 +272,8 @@ const htmlTemplate = (req, reactDom, preloadedState, css, cssString, emotionCss,
         window.nodeEnvSettings.PARKING_STATISTICS_URL = "${process.env.PARKING_STATISTICS_URL}";
         window.nodeEnvSettings.ROADWORKS_API = "${process.env.ROADWORKS_API}";
         window.nodeEnvSettings.RAILWAYS_API = "${process.env.RAILWAYS_API}";
-        window.nodeEnvSettings.FEATURE_SERVICEMAP_PAGE_TRACKING = "${process.env.FEATURE_SERVICEMAP_PAGE_TRACKING}";
         window.nodeEnvSettings.AIR_MONITORING_API = "${process.env.AIR_MONITORING_API}";
+        window.nodeEnvSettings.FEATURE_SERVICEMAP_PAGE_TRACKING = "${process.env.FEATURE_SERVICEMAP_PAGE_TRACKING}";
 
         window.appVersion = {};
         window.appVersion.tag = "${versionTag}";
