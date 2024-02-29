7a07557621f692362a3f5f4ce5bc08726c6f0874
juhomakkonen <juho.makkonen@turku.fi>
2024-02-29 10:32:50 +0200

    Update styles of charging stations' content view

--- src/components/MobilityPlatform/ChargerStationMarkers/components/ChargerStationContent/ChargerStationContent.js
+++ src/components/MobilityPlatform/ChargerStationMarkers/components/ChargerStationContent/ChargerStationContent.js
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from '@emotion/styled';
+import { useIntl } from 'react-intl';
 import TextComponent from '../../../TextComponent';
 
-const ChargerStationContent = ({ classes, intl, station }) => {
+const ChargerStationContent = ({ station }) => {
+  const intl = useIntl();
+
   const titleTypo = (messageId, props = {}) => (
     <div {...props}>
       <Typography variant="subtitle2" component="h3">
@@ -17,9 +22,7 @@ const ChargerStationContent = ({ classes, intl, station }) => {
 
   const singleValTypo = (messageId, value, props = {}) => (
     <div {...props}>
-      <Typography variant="body2">
-        {intl.formatMessage({ id: messageId }, { value })}
-      </Typography>
+      <Typography variant="body2">{intl.formatMessage({ id: messageId }, { value })}</Typography>
     </div>
   );
 
@@ -35,7 +38,7 @@ const ChargerStationContent = ({ classes, intl, station }) => {
     sv: station.address_sv,
   };
 
-  const renderAdministrator = (item) => {
+  const renderAdministrator = item => {
     const stationAdmin = {
       fi: item.fi,
       en: item.en,
@@ -66,38 +69,73 @@ const ChargerStationContent = ({ classes, intl, station }) => {
     <>
       {station.address ? <TextComponent messageId="mobilityPlatform.content.address" textObj={stationAddress} /> : null}
       {station.extra.administrator.fi !== '' ? renderAdministrator(station.extra.administrator) : null}
-      {renderPayment(station.extra.payment, { className: classes.margin })}
-      {titleTypo('mobilityPlatform.content.chargersTitle', { className: classes.margin })}
+      {renderPayment(station.extra.payment, { style: { margin: '3px' } })}
+      {titleTypo('mobilityPlatform.content.chargersTitle', { style: { margin: '3px' } })}
       {station.extra.chargers && station.extra.chargers.length > 0
         ? station.extra.chargers.map(charger => (
-          <div key={`${charger.plug}${charger.power}${charger.number}`} className={classes.contentInner}>
+          <StyledContentInner key={`${charger.plug}${charger.power}${charger.number}`}>
             {singleValTypo('mobilityPlatform.content.cgsType', charger.plug)}
             {singleValTypo('mobilityPlatform.content.count', charger.number)}
             <Typography variant="body2">
               {intl.formatMessage({ id: 'mobilityPlatform.content.power' }, { value: charger.power })}
             </Typography>
-          </div>
+          </StyledContentInner>
         ))
         : null}
     </>
   );
 
   return (
-    <div className={classes.container}>
-      <div className={classes.headerContainer}>
+    <StyledContainer>
+      <StyledHeaderContainer>
         <TextComponent textObj={stationName} isTitle />
-      </div>
-      <div className={classes.textContainer}>
-        {chargerStationInfo}
-      </div>
-    </div>
+      </StyledHeaderContainer>
+      <StyledTextContainer>{chargerStationInfo}</StyledTextContainer>
+    </StyledContainer>
   );
 };
 
+const StyledContainer = styled.div(({ theme }) => ({
+  margin: theme.spacing(1),
+}));
+
+const StyledHeaderContainer = styled.div(({ theme }) => ({
+  width: '85%',
+  borderBottom: '1px solid #000',
+  paddingBottom: theme.spacing(0.5),
+}));
+
+const StyledTextContainer = styled.div(({ theme }) => ({
+  marginTop: theme.spacing(0.5),
+}));
+
+const StyledContentInner = styled.div(({ theme }) => ({
+  marginLeft: theme.spacing(1),
+  marginBottom: theme.spacing(1),
+}));
+
 ChargerStationContent.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.any).isRequired,
-  intl: PropTypes.objectOf(PropTypes.any).isRequired,
-  station: PropTypes.objectOf(PropTypes.any),
+  station: PropTypes.shape({
+    name: PropTypes.string,
+    name_fi: PropTypes.string,
+    name_en: PropTypes.string,
+    name_sv: PropTypes.string,
+    address: PropTypes.string,
+    address_fi: PropTypes.string,
+    address_en: PropTypes.string,
+    address_sv: PropTypes.string,
+    extra: PropTypes.shape({
+      administrator: PropTypes.objectOf(PropTypes.string),
+      payment: PropTypes.string,
+      chargers: PropTypes.arrayOf(
+        PropTypes.shape({
+          plug: PropTypes.string,
+          power: PropTypes.string,
+          number: PropTypes.string,
+        }),
+      ),
+    }),
+  }),
 };
 
 ChargerStationContent.defaultProps = {
