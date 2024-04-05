e08a03d3969c708c634e324ed94b6fa545d77fbf
juhomakkonen <juho.makkonen@turku.fi>
2024-04-05 08:52:36 +0300

    Add custom hook that fetches mobility map data

--- src/components/MobilityPlatform/BikeServiceStations/BikeServiceStations.js
+++ src/components/MobilityPlatform/BikeServiceStations/BikeServiceStations.js
@@ -1,18 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import { useSelector } from 'react-redux';
 import bikeServiceIconBw from 'servicemap-ui-turku/assets/icons/contrast/icons-icon_bike_service_station-bw.svg';
 import bikeServiceIcon from 'servicemap-ui-turku/assets/icons/icons-icon_bike_service_station.svg';
 import { useMobilityPlatformContext } from '../../../context/MobilityPlatformContext';
 import { useAccessibleMap } from '../../../redux/selectors/settings';
-import { fetchMobilityMapData } from '../mobilityPlatformRequests/mobilityPlatformRequests';
+import useMobilityDataFetch from '../utils/useMobilityDataFetch';
 import { createIcon, isDataValid, fitToMapBounds } from '../utils/utils';
 import MarkerComponent from '../MarkerComponent';
 import BikeServiceStationContent from './components/BikeServiceStationContent';
 
 const BikeServiceStations = () => {
-  const [bikeServiceStations, setBikeServiceStations] = useState([]);
+  // const [bikeServiceStations, setBikeServiceStations] = useState([]);
 
   const { showBikeServiceStations } = useMobilityPlatformContext();
 
@@ -22,33 +22,37 @@ const BikeServiceStations = () => {
 
   const { icon } = global.L;
 
+  const options = {
+    type_name: 'BikeServiceStation',
+  };
+
   const customIcon = icon(createIcon(useContrast ? bikeServiceIconBw : bikeServiceIcon));
 
-  useEffect(() => {
+  const { data } = useMobilityDataFetch(options, showBikeServiceStations);
+
+  /* useEffect(() => {
     const options = {
       type_name: 'BikeServiceStation',
     };
     if (showBikeServiceStations) {
       fetchMobilityMapData(options, setBikeServiceStations);
     }
-  }, [showBikeServiceStations]);
+  }, [showBikeServiceStations]); */
 
-  const renderData = isDataValid(showBikeServiceStations, bikeServiceStations);
+  const renderData = isDataValid(showBikeServiceStations, data);
 
   useEffect(() => {
-    fitToMapBounds(renderData, bikeServiceStations, map);
-  }, [showBikeServiceStations, bikeServiceStations]);
+    fitToMapBounds(renderData, data, map);
+  }, [showBikeServiceStations, data]);
 
   return (
-    <>
-      {renderData
-        ? bikeServiceStations.map(item => (
-          <MarkerComponent key={item.id} item={item} icon={customIcon}>
-            <BikeServiceStationContent station={item} />
-          </MarkerComponent>
-        ))
-        : null}
-    </>
+    renderData
+      ? data.map(item => (
+        <MarkerComponent key={item.id} item={item} icon={customIcon}>
+          <BikeServiceStationContent station={item} />
+        </MarkerComponent>
+      ))
+      : null
   );
 };
 
--- src/components/MobilityPlatform/GasFillingStationMarkers/GasFillingStationMarkers.js
+++ src/components/MobilityPlatform/GasFillingStationMarkers/GasFillingStationMarkers.js
@@ -1,20 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useMap } from 'react-leaflet';
 import gasFillingIcon from 'servicemap-ui-turku/assets/icons/icons-icon_gas_station.svg';
 import gasFillingIconBw from 'servicemap-ui-turku/assets/icons/contrast/icons-icon_gas_station-bw.svg';
 import { useMobilityPlatformContext } from '../../../context/MobilityPlatformContext';
 import { useAccessibleMap } from '../../../redux/selectors/settings';
-import { fetchMobilityMapData } from '../mobilityPlatformRequests/mobilityPlatformRequests';
 import { createIcon, isDataValid, fitToMapBounds } from '../utils/utils';
+import useMobilityDataFetch from '../utils/useMobilityDataFetch';
 import MarkerComponent from '../MarkerComponent';
 import GasFillingStationContent from './components/GasFillingStationContent';
 
 const GasFillingStationMarkers = () => {
-  const [gasFillingStations, setGasFillingStations] = useState([]);
+  const options = {
+    type_name: 'GasFillingStation',
+  };
 
   const { showGasFillingStations } = useMobilityPlatformContext();
+  const { data } = useMobilityDataFetch(options, showGasFillingStations);
 
   const { icon } = global.L;
 
@@ -22,33 +25,22 @@ const GasFillingStationMarkers = () => {
 
   const gasStationIcon = icon(createIcon(useContrast ? gasFillingIconBw : gasFillingIcon));
 
-  useEffect(() => {
-    const options = {
-      type_name: 'GasFillingStation',
-    };
-    if (showGasFillingStations) {
-      fetchMobilityMapData(options, setGasFillingStations);
-    }
-  }, [showGasFillingStations]);
-
-  const renderData = isDataValid(showGasFillingStations, gasFillingStations);
+  const renderData = isDataValid(showGasFillingStations, data);
 
   const map = useMap();
 
   useEffect(() => {
-    fitToMapBounds(renderData, gasFillingStations, map);
-  }, [showGasFillingStations, gasFillingStations]);
+    fitToMapBounds(renderData, data, map);
+  }, [showGasFillingStations, data]);
 
   return (
-    <>
-      {renderData
-        ? gasFillingStations.map(item => (
-          <MarkerComponent key={item.id} item={item} icon={gasStationIcon}>
-            <GasFillingStationContent station={item} />
-          </MarkerComponent>
-        ))
-        : null}
-    </>
+    renderData
+      ? data.map(item => (
+        <MarkerComponent key={item.id} item={item} icon={gasStationIcon}>
+          <GasFillingStationContent station={item} />
+        </MarkerComponent>
+      ))
+      : null
   );
 };
 
