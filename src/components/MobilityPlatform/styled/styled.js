0d57429d6ac98e968576074b631f2b1ff5ae1912
juhomakkonen <juho.makkonen@turku.fi>
2024-04-16 10:46:19 +0300

    Remove withStyles & use styled components

--- src/components/MobilityPlatform/styled/styled.js
+++ src/components/MobilityPlatform/styled/styled.js
@@ -19,4 +19,29 @@ const StyledContentHeader = styled.div(({ theme }) => ({
   width: '95%',
 }));
 
-export { StyledPopupInner, StyledContentHeader };
+const StyledContainer = styled.div(({ theme }) => ({
+  margin: theme.spacing(1),
+}));
+
+const StyledHeaderContainer = styled.div(({ theme }) => ({
+  width: '85%',
+  borderBottom: '1px solid #000',
+  paddingBottom: theme.spacing(0.5),
+}));
+
+const StyledTextContainer = styled.div(({ theme }) => ({
+  marginTop: theme.spacing(0.5),
+}));
+
+const StyledMargin = styled.div(({ theme }) => ({
+  margin: theme.spacing(0.4),
+}));
+
+export {
+  StyledPopupInner,
+  StyledContentHeader,
+  StyledContainer,
+  StyledHeaderContainer,
+  StyledTextContainer,
+  StyledMargin,
+};
