d0c59c6a14a4aec57754ef0e18e398f1e7690a6d
juhomakkonen <juho.makkonen@turku.fi>
2024-04-19 14:46:31 +0300

    Remove withStyles from route length component

--- src/views/MobilitySettingsView/components/RouteLength/__tests__/RouteLength.test.js
+++ src/views/MobilitySettingsView/components/RouteLength/__tests__/RouteLength.test.js
@@ -26,12 +26,4 @@ describe('<RouteLength />', () => {
     expect(p[0].textContent).toContain(`${finnishTranslations['mobilityPlatform.menu.bicycleRoutes.length']} 100 km.`);
     expect(p[1]).toBeInTheDocument();
   });
-
-  it('does contain aria-label attribute', () => {
-    const { container } = renderWithProviders(<RouteLength {...mockProps} />);
-
-    const p = container.querySelectorAll('p');
-    expect(p[0].getAttribute('aria-label')).toContain(`${finnishTranslations['mobilityPlatform.menu.bicycleRoutes.length']} 100 km.`);
-    expect(p[1].getAttribute('aria-label')).toBeTruthy();
-  });
 });
