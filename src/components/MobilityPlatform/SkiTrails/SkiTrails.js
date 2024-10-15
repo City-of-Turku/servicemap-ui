f12ae3083e5e02a3499e190a7c0c4b0051d3ac1b
SanttuA <santtualatalo@gmail.com>
2024-10-15 13:22:49 +0300

    Updated ski trails map rendering

--- src/components/MobilityPlatform/SkiTrails/SkiTrails.js
+++ src/components/MobilityPlatform/SkiTrails/SkiTrails.js
@@ -19,17 +19,6 @@ const SkiTrails = () => {
 
   const { Polyline } = global.rL;
 
-  const getPathOptions = () => {
-    const rgba = [15, 115, 6, 255];
-    const pattern = '8 3 8';
-    return {
-      color: `rgba(${rgba})`,
-      fillOpacity: 0.3,
-      dashArray: pattern,
-      weight: 5,
-    };
-  };
-
   const currentDate = new Date();
   // TODO: handle correct time options
   const yesterDay = format(subDays(currentDate, 1), 'yyyy-MM-dd HH:mm:ss');
@@ -50,15 +39,6 @@ const SkiTrails = () => {
 
   const validateData = inputData => inputData && inputData.length > 0;
   let isDataValid = false;
-  // TODO: swapping can be done in coords get util
-  const swapCoords = coordsData => {
-    const isValid = validateData(coordsData);
-    if (isValid) {
-      const swapped = coordsData.map(item => [item[1], item[0]]);
-      return swapped;
-    }
-    return coordsData;
-  };
 
   useEffect(() => {
     if (!isDataValid) {
@@ -67,32 +47,13 @@ const SkiTrails = () => {
   }, [isDataValid, skiTrailsPeriod, isActiveSkiTrails, setIsActiveSkiTrails]);
 
   const renderData = inputData => {
-    // TODO: below dont work. Handle geometries.geometry string
-    // geometry: "SRID=4326;LINESTRING(x y, x y, x y)"
-    // handle possible empty data?
-    const filtered = inputData.reduce((acc, curr) => {
-      if (curr.geometry_type === 'LineString') {
-        acc.push(curr);
-      }
-      return acc;
-    }, []);
-    const mapped = inputData.map(item => {
-      // eslint-disable-next-line camelcase
-      const { id, last_imported_time, geometries } = item;
-      const coordinates = getCoordinates(geometries[0].geometry);
-      // eslint-disable-next-line camelcase
-      return { id, last_imported_time, coordinates };
-    });
-
-    isDataValid = validateData(mapped);
-
+    isDataValid = validateData(inputData);
     if (isDataValid) {
-      return mapped.map((item, i) => (
+      return inputData.map((item, i) => (
         <Polyline
           // eslint-disable-next-line react/no-array-index-key
           key={`${item.id}${item.last_imported_time}${i}`}
-          // pathOptions={getPathOptions(item.events[0])}
-          positions={swapCoords(item.coordinates)}
+          positions={getCoordinates(item.geometries[0]?.geometry)}
         />
       ));
     }
--- src/components/MobilityPlatform/SkiTrails/utils.js
+++ src/components/MobilityPlatform/SkiTrails/utils.js
@@ -3,9 +3,12 @@
 /**
  * Parses given linestring data and returns its coordinates
  * @param {string} s input linestring data with coordinates
- * @returns array of coordinate arrays [x, y]
+ * @returns array of coordinate arrays [y, x]
  */
 export function getCoordinates(s) {
+  if (!s) {
+    return [];
+  }
   const pattern = /LINESTRING \(([^)]+)\)/;
 
   const match = s.match(pattern);
@@ -16,9 +19,10 @@ export function getCoordinates(s) {
     // Split each pair into numbers and create an array of arrays [x, y]
     const coordinates = coordPairs.map(pair => {
       const [x, y] = pair.split(' ').map(Number);
-      return [x, y];
+      // swap coordinate order for map
+      return [y, x];
     });
     return coordinates;
   }
-  return null;
+  return [];
 }
