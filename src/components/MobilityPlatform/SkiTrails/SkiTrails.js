5733af5dc139c1221ee55e158a1265686ae2e79e
SanttuA <santtualatalo@gmail.com>
2024-10-25 12:49:03 +0300

    Added ski trail contrast line and optimizations

--- src/components/MobilityPlatform/SkiTrails/SkiTrails.js
+++ src/components/MobilityPlatform/SkiTrails/SkiTrails.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { format, subDays } from 'date-fns';
+import { useSelector } from 'react-redux';
 import { useMobilityPlatformContext } from '../../../context/MobilityPlatformContext';
 import { fetchMaintenanceData } from '../mobilityPlatformRequests/mobilityPlatformRequests';
 import { getCoordinates } from './utils';
-
-// TODO: check SnowPlows and build
+import { useAccessibleMap } from '../../../redux/selectors/settings';
+import { blackOptionsBase, whiteOptionsBase } from '../utils/utils';
 
 const SkiTrails = () => {
+  const [loading, setLoading] = useState(true);
   const [skiTrails1Day, setSkiTrails1Day] = useState([]);
   const [skiTrails2Day, setSkiTrails2Day] = useState([]);
   const [skiTrails3Day, setSkiTrails3Day] = useState([]);
@@ -16,7 +18,7 @@ const SkiTrails = () => {
   const {
     skiTrailsPeriod, showSkiTrails, isActiveSkiTrails, setIsActiveSkiTrails,
   } = useMobilityPlatformContext();
-
+  const useContrast = useSelector(useAccessibleMap);
   const { Polyline } = global.rL;
 
   const currentDate = new Date();
@@ -30,51 +32,74 @@ const SkiTrails = () => {
   const createQuery = dateItem => `unit_maintenance?page_size=50000&target__iexact=ski_trail&maintained_at__gte=${dateItem}`;
 
   useEffect(() => {
-    fetchMaintenanceData(createQuery(yesterDay), setSkiTrails1Day);
-    fetchMaintenanceData(createQuery(twoDays), setSkiTrails2Day);
-    fetchMaintenanceData(createQuery(threeDays), setSkiTrails3Day);
-    fetchMaintenanceData(createQuery(fourDays), setSkiTrails4Day);
-    fetchMaintenanceData(createQuery(fiveDays), setSkiTrails5Day);
+    const fetchAllData = async () => {
+      await Promise.all([
+        fetchMaintenanceData(createQuery(yesterDay), setSkiTrails1Day),
+        fetchMaintenanceData(createQuery(twoDays), setSkiTrails2Day),
+        fetchMaintenanceData(createQuery(threeDays), setSkiTrails3Day),
+        fetchMaintenanceData(createQuery(fourDays), setSkiTrails4Day),
+        fetchMaintenanceData(createQuery(fiveDays), setSkiTrails5Day),
+      ]);
+
+      setLoading(false);
+    };
+
+    fetchAllData();
   }, []);
 
   const validateData = inputData => inputData && inputData.length > 0;
   let isDataValid = false;
 
   useEffect(() => {
-    if (!isDataValid) {
+    if (!isDataValid && isActiveSkiTrails) {
       setIsActiveSkiTrails(false);
-    } else setIsActiveSkiTrails(true);
-  }, [isDataValid, skiTrailsPeriod, isActiveSkiTrails, setIsActiveSkiTrails]);
+    } else if (isDataValid && !isActiveSkiTrails) {
+      setIsActiveSkiTrails(true);
+    }
+  }, [isDataValid, skiTrailsPeriod, isActiveSkiTrails, setIsActiveSkiTrails, loading]);
 
   const renderData = inputData => {
     isDataValid = validateData(inputData);
     if (isDataValid) {
-      return inputData.map((item, i) => (
-        <Polyline
-          // eslint-disable-next-line react/no-array-index-key
-          key={`${item.id}${item.last_imported_time}${i}`}
-          positions={getCoordinates(item.geometries[0]?.geometry)}
-        />
-      ));
+      return inputData.map((item, i) => {
+        const coordinates = getCoordinates(item.geometries[0]?.geometry);
+
+        return (
+          useContrast ? (
+            // eslint-disable-next-line react/no-array-index-key
+            <React.Fragment key={`${item.id}${item.last_imported_time}${i}`}>
+              <Polyline weight={8} pathOptions={whiteOptionsBase({ dashArray: null })} positions={coordinates} />
+              <Polyline weight={4} pathOptions={blackOptionsBase({ dashArray: '2 9 9 9' })} positions={coordinates} />
+            </React.Fragment>
+          ) : (
+            <Polyline
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${item.id}${item.last_imported_time}${i}`}
+              positions={coordinates}
+            />
+          )
+        );
+      });
     }
+
     return null;
   };
 
   const renderSkiTrails = () => {
-    const works = new Map();
+    const trails = new Map();
     // TODO: handle different time options
-    works.set('1day', skiTrails1Day);
-    works.set('2days', skiTrails2Day);
-    works.set('3days', skiTrails3Day);
-    works.set('4days', skiTrails4Day);
-    works.set('5days', skiTrails5Day);
-    if (works.has(skiTrailsPeriod)) {
-      return renderData(works.get(skiTrailsPeriod));
+    trails.set('1day', skiTrails1Day);
+    trails.set('2days', skiTrails2Day);
+    trails.set('3days', skiTrails3Day);
+    trails.set('4days', skiTrails4Day);
+    trails.set('5days', skiTrails5Day);
+    if (trails.has(skiTrailsPeriod)) {
+      return renderData(trails.get(skiTrailsPeriod));
     }
     return null;
   };
 
-  return showSkiTrails ? renderSkiTrails() : null;
+  return showSkiTrails && !loading ? renderSkiTrails() : null;
 };
 
 export default SkiTrails;
